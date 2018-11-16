b2f840a6c3c64fd592c9bd52f9602562bf5eb860
Robert Katzki <katzki@ubilabs.net>
2018-11-16 10:46:02 +0100

    feat: use helmet for security

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import helmet from 'helmet';
 import {google} from 'googleapis';
 import credentials from './credentials.json';
 import DoExport from './do-export';
@@ -18,6 +19,7 @@ const scope = [
 ];
 
 app.set('view engine', 'pug');
+app.use(helmet());
 app.use(express.static('assets'));
 
 app.get('/', (req, res) => {
