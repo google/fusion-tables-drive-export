4c4c248d3bc11a1af7d029cac6c920d008716a98
Robert Katzki <katzki@ubilabs.net>
2019-02-21 15:31:53 +0100

    feat(visualizer): load file from URL hash param

--- visualizer-src/scripts/fetch-data.ts
+++ visualizer-src/scripts/fetch-data.ts
@@ -1,16 +1,27 @@
 import Papa from 'papaparse';
 
-const id = '1nlXO19BelHqPJskSxPO1RO5-eG52cAK3ehybW6XzX_c';
-const url = `https://docs.google.com/spreadsheets/d/${id}/gviz/tq?tqx=out:csv`;
-const url2 = `https://docs.google.com/spreadsheets/d/${id}/export?format=csv`;
-
 /**
  * Fetch a CSV and return it as JSON
  */
 export default async function(): Promise<any> {
+  const {hash} = document.location;
+
+  if (!hash || !hash.startsWith('#file=')) {
+    return null;
+  }
+
+  const id = hash.replace('#file=', '');
+  const url = `https://docs.google.com/spreadsheets/d/${id}/export?format=csv`;
+
   try {
+
     const response = await fetch(url);
     const csv = await response.text();
+
+    if (csv.startsWith('<!DOCTYPE html>')) {
+      return null;
+    }
+
     const parsed = Papa.parse(csv);
     return parsed.data;
   } catch (error) {
