d0da7f18dc3bb099e282e52eb0b8778123b2ab98
Robert Katzki <katzki@ubilabs.net>
2019-02-26 16:34:11 +0100

    feat(visualizer): prevent click-through

--- visualizer-src/scripts/init-infowindow.ts
+++ visualizer-src/scripts/init-infowindow.ts
@@ -5,8 +5,6 @@ import {LAYER_ID} from './config';
  * Initialize an infowindow that shows the clicked data
  */
 export default function(map: google.maps.Map, deck: Deck): void {
-  const dummyOverlay = createDummyOverlay(map);
-
   window.addEventListener('mousemove', event => {
     if (!(deck as any).layerManager) {
       return;
@@ -17,16 +15,15 @@ export default function(map: google.maps.Map, deck: Deck): void {
     document.body.classList.toggle('cursor-pointer', picked);
   });
 
-  window.addEventListener('click', event => {
-    const {clientX: x, clientY: y} = event;
-    // PREVENT BUBBLING WHEN NOT CLICKING MAP
+  map.addListener('click', event => {
+    const {latLng, pixel} = event;
+    const {x, y} = pixel;
     const picked = deck.pickObject({x, y, radius: 4, layerIds: [LAYER_ID]});
 
     if (picked) {
-      const position = dummyOverlay
-        .getProjection()
-        .fromContainerPixelToLatLng(new google.maps.Point(x, y));
-      openInfowindow(infowindow, map, picked.object, position);
+      openInfowindow(infowindow, map, picked.object, latLng);
+    } else {
+      infowindow.close();
     }
   });
 
@@ -52,24 +49,4 @@ function openInfowindow(
   infowindow.setContent(JSON.stringify(feature.properties));
   infowindow.setPosition(position);
   infowindow.open(map);
-}
-
-/**
- * Create a dummy overlay to calculate the position
- */
-function createDummyOverlay(map: google.maps.Map): google.maps.OverlayView {
-  if (!map) {
-    return;
-  }
-
-  function Dummy(internalMap: google.maps.Map) {
-    this.setMap(internalMap);
-  }
-
-  Dummy.prototype = new google.maps.OverlayView();
-  Dummy.prototype.draw = () => undefined;
-  Dummy.prototype.onAdd = () => undefined;
-  Dummy.prototype.onRemove = () => undefined;
-
-  return new Dummy(map);
 }
