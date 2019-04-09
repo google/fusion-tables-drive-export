ada2a275b79d60c0b6f12bdbe3236c98b42db875
Robert Katzki <katzki@ubilabs.net>
2019-04-09 12:26:05 +0200

    feat(server): create a subfolder for each export

--- server-src/config.ts
+++ server-src/config.ts
@@ -1,1 +1,12 @@
-export const DRIVE_FOLDER = 'FT-archive';
+export const DRIVE_ARCHIVE_FOLDER = 'ft-archive';
+
+export const getDriveSubfolderName = () => {
+  const now = new Date();
+  const Y = now.getFullYear();
+  const M = String(now.getMonth() + 1).padStart(2, '0');
+  const D = String(now.getDay()).padStart(2, '0');
+  const h = now.getHours();
+  const m = now.getMinutes();
+
+  return `export-${Y}${M}${D}-${h}${m}`;
+};
