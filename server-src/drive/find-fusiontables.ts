4be8d85e3d335c423d6bf4c45e61cca814a7a844
Robert Katzki <katzki@ubilabs.net>
2019-04-26 14:47:42 +0200

    feat(server): search Fusiontables on multiple pages

--- server-src/drive/find-fusiontables.ts
+++ server-src/drive/find-fusiontables.ts
@@ -4,6 +4,11 @@ import {ITable} from '../interfaces/table';
 
 const drive = google.drive('v3');
 
+interface IGetTableFiles {
+  nextPageToken?: string;
+  tables: ITable[];
+}
+
 /**
  * Find all Fusiontables owned by user
  */
@@ -12,11 +17,39 @@ export default async function(
   ids?: string[]
 ): Promise<ITable[]> {
   try {
+    let allTables: ITable[] = [];
+    let pageToken: string | undefined;
+
+    do {
+      const {tables, nextPageToken} = await getTableFiles(auth, pageToken);
+      pageToken = nextPageToken;
+      allTables = [...allTables, ...tables];
+    } while (pageToken);
+
+    if (ids) {
+      allTables = allTables.filter(table => ids.includes(table.id as string));
+    }
+
+    return allTables;
+  } catch (error) {
+    throw error;
+  }
+}
+
+/**
+ * Get all Fusiontables owned by user
+ */
+async function getTableFiles(
+  auth: OAuth2Client,
+  nextPageToken?: string
+): Promise<IGetTableFiles> {
+  try {
     const response = await drive.files.list({
       auth,
       orderBy: 'name',
       pageSize: 1000,
-      fields: 'files(id,name,permissions)',
+      pageToken: nextPageToken,
+      fields: 'nextPageToken,files(id,name,permissions)',
       q:
         // tslint:disable quotemark
         "'me' in owners and " +
@@ -24,17 +57,11 @@ export default async function(
         'trashed = false'
         // tslint:enable quotemark
     });
-    let files = response.data.files;
 
-    if (!files) {
-      return [];
-    }
-
-    if (ids) {
-      files = files.filter(file => ids.includes(file.id as string));
-    }
-
-    return files as ITable[];
+    return {
+      nextPageToken: response.data.nextPageToken,
+      tables: (response.data.files as ITable[]) || []
+    };
   } catch (error) {
     throw error;
   }
