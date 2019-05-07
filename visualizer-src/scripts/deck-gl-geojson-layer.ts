3a0d83092ae5b970e663b3e47b8e39b91486ff34
Robert Katzki <katzki@ubilabs.net>
2019-05-07 15:17:20 +0200

    feat(visualizer): apply style passed in from URL

--- visualizer-src/scripts/deck-gl-geojson-layer.ts
+++ visualizer-src/scripts/deck-gl-geojson-layer.ts
@@ -1,22 +1,67 @@
 import {GeoJsonLayer} from '@deck.gl/layers';
 import {LAYER_ID} from './config';
+import {IStyle} from '../../server-src/interfaces/style';
+import getGeojsonType from './get-geojson-type';
+import hexaToRgba from './hexa-to-rgba';
 
 /**
  * Create a GeoJSON layer from the data
  * Converts the WKT data to GeoJSON
  */
-export default function(data: string[][]): GeoJsonLayer {
+export default function(data: string[][], style?: IStyle): GeoJsonLayer {
   return new GeoJsonLayer({
     id: LAYER_ID,
     data: createGeojsonFromData(data),
     pickable: true,
     stroked: true,
     filled: true,
+    lineWidthUnits: 'pixels',
     lineWidthMinPixels: 1,
     pointRadiusMinPixels: 3,
-    getFillColor: [239, 83, 80, 97],
-    getLineColor: [239, 83, 80, 214],
-    getRadius: 100
+    getRadius: style.marker.size === 'large' ? 250 : 100,
+    getFillColor: (d: GeoJSON.GeoJSON) => {
+      const type = getGeojsonType(d);
+
+      if (type === 'marker') {
+        return hexaToRgba(style.marker.fillColor);
+      }
+
+      if (type === 'polygon') {
+        return hexaToRgba(style.polygon.fillColor);
+      }
+
+      return [239, 83, 80, 97];
+    },
+    getLineColor: (d: GeoJSON.GeoJSON) => {
+      const type = getGeojsonType(d);
+
+      if (type === 'marker') {
+        return [102, 102, 102, 255];
+      }
+
+      if (type === 'polyline') {
+        return hexaToRgba(style.polyline.strokeColor);
+      }
+
+      if (type === 'polygon') {
+        return hexaToRgba(style.polygon.strokeColor);
+      }
+
+      return [239, 83, 80, 214];
+    },
+    getLineWidth: (d: GeoJSON.GeoJSON) => {
+      const type = getGeojsonType(d);
+
+      if (type === 'polyline') {
+        return style.polyline.strokeWeight;
+      }
+
+      if (type === 'polygon') {
+        return style.polygon.strokeWeight;
+      }
+
+      return 1;
+    }
   });
 }
 
