7cf5083df9211b994339d2d80583721c50f7224d
Robert Katzki <katzki@ubilabs.net>
2019-05-23 11:33:07 +0200

    feat: give buttons a loading state

--- server-static-src/main.ts
+++ server-static-src/main.ts
@@ -3,3 +3,13 @@ import './export-in-progress';
 import './main.styl';
 
 document.body.classList.remove('no-js');
+
+/* tslint:disable prefer-for-of */
+
+const $buttons = document.querySelectorAll('.button');
+for (let i = 0; i < $buttons.length; ++i) {
+  const $button = $buttons[i];
+  $button.addEventListener('click', () => {
+    $button.classList.add('button--loading');
+  });
+}
