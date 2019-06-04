5b2834266542fa8d5d1ce6c2314e8ec986565c23
Robert Katzki <katzki@ubilabs.net>
2019-06-04 11:20:15 +0200

    feat: limit Google Drive scope

--- server-src/lib/auth.ts
+++ server-src/lib/auth.ts
@@ -5,7 +5,8 @@ import {Request} from 'express';
 
 const SCOPE = [
   'https://www.googleapis.com/auth/fusiontables.readonly',
-  'https://www.googleapis.com/auth/drive'
+  'https://www.googleapis.com/auth/drive.file',
+  'https://www.googleapis.com/auth/drive.metadata'
 ];
 
 export function getOAuthClient(req: Request): OAuth2Client {
